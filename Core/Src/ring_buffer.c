ec417d33db4b65dd307c4887202a27e8d5be5e94
diarboleda <diarboledac@unal.edu.co>
2024-08-23 13:36:08 -0500

    Se realizaron las funciones correspondientes  ideadas en clase,del mismo modo se implemento la 2da parte de la  tarea que consistia en que al momento de ingresar el numero de documento el sistema regresa el nombre de dicha persona

---
+++ Core/Src/ring_buffer.c
@@ -0,0 +1,72 @@
+#include "ring_buffer.h"
+
+#define capacity (10) // capacidad del buffer
+uint8_t ring_buffer[capacity]; // buffer
+uint8_t head_ptr = 0; // cabeza del buffer
+uint8_t tail_ptr = 0; // cola del buffer
+uint8_t is_full = 0; // indicador de buffer lleno
+
+void ring_buffer_reset(void) {
+    head_ptr = 0;
+    tail_ptr = 0;
+    is_full = 0;
+}
+
+uint8_t ring_buffer_size(void) {
+    uint8_t size = 0;
+
+    if (is_full) {
+        size = capacity;
+    } else if (head_ptr >= tail_ptr) {
+        size = head_ptr - tail_ptr;
+    } else {
+        size = capacity + head_ptr - tail_ptr;
+    }
+
+    return size;
+}
+
+uint8_t ring_buffer_is_full(void) {
+    return is_full;
+}
+
+uint8_t ring_buffer_is_empty(void) {
+    return (!is_full && (head_ptr == tail_ptr));
+}
+
+void ring_buffer_write(uint8_t data)
+{
+	ring_buffer[head_ptr] = data;
+	head_ptr = head_ptr + 1;
+
+	if (head_ptr >= capacity) { // si la cabeza llega al final de la memoria
+	  head_ptr = 0;
+	}
+
+	if (is_full != 0) { // si se pierden datos viejos
+	  tail_ptr = tail_ptr + 1;
+	}
+
+	if (tail_ptr >= capacity) { // si la cola llega al final de la memoria
+	  tail_ptr = 0;
+	}
+
+	if (head_ptr == tail_ptr) { // si la cabeza alcanza la cola
+	  is_full = 1;
+	}
+}
+
+uint8_t ring_buffer_read(uint8_t *data) // 0x20
+{
+	if ((is_full != 0) || (head_ptr != tail_ptr)) { // data available
+		*data = ring_buffer[tail_ptr]; // add: 0x20, val: buffer
+		tail_ptr = tail_ptr + 1;
+		if (tail_ptr >= capacity) {
+			tail_ptr = 0;
+		}
+		is_full = 0;
+
+		return 1; // buffer con datos
+	}
+	return 0; // buffer vacio
+}
